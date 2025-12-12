c8bd557c7cda39035f86f3556de284a9ce19ee21
Sofia.Povarnitsyna <spo@item.expert>
2025-12-12 15:34:36 +0300

    PrimeVue

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,3 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+<Checkbox :model-value="true" binary />
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primeuix/themes/aura';
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@primevue/nuxt-module'
+  ],
+  primevue: {
+    autoImport: true,
+    options: {
+      theme: {
+        preset: Aura
+      }
+    }
+  }
 })
