24c20418b5d3ee049d4cf9746748da2c0633b454
Sofia.Povarnitsyna <spo@item.expert>
2026-04-03 19:27:29 +0300

    Создала компонент для переключения темы

---
+++ app/components/c-theme.vue
@@ -0,0 +1,27 @@
+<template>
+  <ToggleSwitch
+      v-model="checked"
+  >
+    <template #handle="{ checked }">
+      <span
+          v-if="checked"
+          class="material-icons text-[12px]! leading-[11px]!"
+      >
+        brightness_7
+      </span>
+
+      <span
+          v-else
+          class="material-icons text-[12px]! leading-[11px]!"
+      >
+        nightlight
+      </span>
+    </template>
+  </ToggleSwitch>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const checked = ref(false);
+</script>
